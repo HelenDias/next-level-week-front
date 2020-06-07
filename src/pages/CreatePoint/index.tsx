82e7c3c8cdb4c64548acf11b780e0a7d65f763b5
Helen Dias <helendias.hd@gmail.com>
2020-06-07 19:10:57 -0300

    :lipstick: Add header to CreatePoint component

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import './styles.css';
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiArrowDownLeft } from 'react-icons/fi';
 
 const CreatePoint = () => {
   return(
-    <h1>Helloo Universe</h1>
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Logo Ecoleta" />
+
+        <Link to="/">
+          <FiArrowDownLeft />
+          Voltar para home
+        </Link>
+      </header>
+    </div>
   );
 }
 
