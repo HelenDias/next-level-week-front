5378a83dadc8aa4430b5250fcee3dc0a59f52dbc
Helen Dias <helendias.hd@gmail.com>
2020-06-07 18:16:34 -0300

    :fire: Clear App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0); // [valor do estado, func para att]
-
-  function handleClick() {
-    setCounter(counter + 1);
-  }
-
   return (
     <div>
-      <Header title={`Ecoleta ${counter}`} />
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleClick}>Clique aqui</button>
+      <h1>Hi there</h1>
     </div>
   );
 }
