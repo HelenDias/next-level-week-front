81bcba730944ac68f4a41f863db4759fec10a7ea
Helen Dias <helendias.hd@gmail.com>
2020-06-07 19:33:14 -0300

    :lipstick: Add button to create point

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -149,6 +149,10 @@ const CreatePoint = () => {
             </li>
           </ul>
         </fieldset>
+
+        <button type="submit">
+          Cadastrar ponto de coleta
+        </button>
       </form>
     </div>
   );
