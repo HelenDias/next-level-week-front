14065e670bb806d95bc83987b718a2041965f17d
Helen Dias <helendias.hd@gmail.com>
2020-06-07 17:58:03 -0300

    :sparkles: Add header file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+
+      <h1>Oi</h1>
+    </div>
   );
 }
 
