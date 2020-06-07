f55cbcd073f7ded1416b2f8a70faba76f8d69bdb
Helen Dias <helendias.hd@gmail.com>
2020-06-07 19:05:26 -0300

    :building_construction: Change route link of button from <a> to <Link>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return(
@@ -15,12 +16,12 @@ const Home = () => {
           <h1>Seu marketplace de coleta de resíduos</h1>
           <p>Ajudamos pessoas a encontrar pontos de coleta de forma eficiente</p>
 
-          <a href="/create-point">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
