d459b652792e783e3bbf38e240e0375346a71a11
Helen Dias <helendias.hd@gmail.com>
2020-06-07 22:42:25 -0300

    :art: Add select items function

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -28,8 +28,9 @@ const CreatePoint = () => {
   const [cities, setCities] = useState<string[]>([]);
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
-  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
 
   const [formData, setFormData] = useState({
     name: '',
@@ -59,6 +60,18 @@ const CreatePoint = () => {
       ...formData,
       [name]: value,
     });
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      return setSelectedItems(filteredItems);
+    }
+
+    return setSelectedItems([ ...selectedItems, id ]);
   }
 
   useEffect(() => {
@@ -261,7 +274,11 @@ const CreatePoint = () => {
           <ul className="items-grid">
             {
               items.map(item => (
-                <li key={item.id}>
+                <li
+                  key={item.id}
+                  onClick={() => handleSelectItem(item.id)}
+                  className={selectedItems.includes(item.id) ? 'selected' : ''}
+                >
                   <img src={item.image} alt={item.title} />
                   <span>
                     {item.title}
